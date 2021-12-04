2ade53bf7248614356b61f7898367222ea982cdd
micos <marcosguilhermefh@gmail.com>
2021-12-04 16:50:02 -0300

    ajustar conexão com banco de dados

--- models/database.py
+++ models/database.py
@@ -1,12 +1,12 @@
 import pymongo
 from dotenv import dotenv_values
-import urllib
+import urllib.parse
 config = dotenv_values(".env")
 
 class Connect:
     def __init__(self):
         if config["MONGO_USER"] and config["MONGO_PASSWORD"]:
-            self.conn_str = f'mongodb://{config["MONGO_USER"]}:{urllib.quote(config["MONGO_PASSWORD"])}@{config["MONGO_ADDRESS"]}:{config["MONGO_PORT"]}'
+            self.conn_str = f'mongodb://{config["MONGO_USER"]}:{urllib.parse.quote(config["MONGO_PASSWORD"],safe= "" )}@{config["MONGO_ADDRESS"]}:{config["MONGO_PORT"]}'
         else:
             self.conn_str = f'mongodb://{config["MONGO_ADDRESS"]}:{config["MONGO_PORT"]}'
         self.client = pymongo.MongoClient(self.conn_str)
