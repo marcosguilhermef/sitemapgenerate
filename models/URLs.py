f0a06f07a1d3e0ecb4e67b2876dff11a17d67d4e
verdinhos <marcosguilhermefh@gmail.com>
2022-02-12 06:07:54 -0300

    ajustes para producao

--- models/URLs.py
+++ models/URLs.py
@@ -6,6 +6,9 @@ class URLs(models.database.Connect):
     def __init__(self):
         super().__init__()
         self.collection = self.database["URLs"]
+    def select_all_urls(self):
+        arr = self.collection.find({})
+        return arr
 
     def select_all_active_urls(self):
         arr = self.collection.find({"ativo": True})
